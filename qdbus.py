4ce61f9792641def4b91f8199995283e516bc9b2
ybist <lei0324@gmail.com>
2020-12-25 17:42:17 +0800

    Update qdbus.py

--- qdbus.py
+++ qdbus.py
@@ -1,131 +1,112 @@
 #! /usr/bin/python3
 #-*-coding:UTF-8-*-
 # 青岛公交查询
-# Author: 2997
+# Author: 2997@YBZN
 
 import requests,time
+import configparser
 
 #33路  隆德路②--南京路宁夏路①
-bus_num1 = '33'
-#begin_station1 =  '隆德路②'
-#wait_station1 = '南京路宁夏路①'
-begin_station1 = '北岭'
-wait_station1 = '山东路南宁路'
-'''
-#301路 高邮湖路  四川路滋阳路② - 万里江茶园
-bus_num1 = '301'
-#begin_station1 =  "四川路滋阳路②"
-begin_station1 = '万里江茶园'
-wait_station1 = '高邮湖路'
+rid = '33'
+#departure_station =  '隆德路②'
+#stop_station = '南京路宁夏路①'
+departure_station = '北岭'
+stop_station = '山东路南宁路'
 
-#223路 高邮湖路  东平路-刘家下庄
-bus_num2 = '223'
-#begin_station2 =  "刘家下庄"
-begin_station2 = '东平路②'
-wait_station2 = '高邮湖路'
-'''
 #获取需要的线路公交站点信息
-def get_bus_info(bus_num,begin_station,wait_station):
-    url = 'http://bus.qingdaonews.com/m/detail.php?rid=' + bus_num + '&isjson=1'
+def get_bus_info(rid,departure_station,stop_station):
+    url = 'http://bus.qingdaonews.com/m/detail.php?rid=' + rid + '&isjson=1'
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 QIHU 360SE'
     }
     response=requests.get(url, headers=headers)
     response.encoding='utf-8'
-    time.sleep(2)
+
     #Unicode转中文
-    list_right = (str(response.text).replace('u\'','\'')).replace('null', '\65e0')
+    list_convert = (str(response.text).replace('u\'','\'')).replace('null', '\65e0')
     #转成列表
-    station_info = eval(list_right.encode('utf-8').decode("unicode-escape"))
-    
+    station_info = eval(list_convert.encode('utf-8').decode("unicode-escape"))
+
     if station_info['stations']:
         rid = station_info['rid']
         segment_id = station_info['stations'][0]['segment_id']
         main_station = station_info['stations'][0]['station_name']
         all_station_num = len(station_info['stations'])
         
-        if begin_station == main_station:
+        if departure_station == main_station:
             for i in range(0,all_station_num-1):
-                if wait_station == station_info['stations'][i]['station_name']:
+                if stop_station == station_info['stations'][i]['station_name']:
                     station_id = station_info['stations'][i]['station_id']
                     break
-            get_bus_detail(begin_station,wait_station,rid,segment_id,station_id)                
+            get_bus_detail(departure_station,stop_station,rid,segment_id,station_id)                
         else: 
             for i in range(0,all_station_num-1):
-                if ('MO' == station_info['stations'][i]['station_direct']) and (wait_station == station_info['stations'][i]['station_name']):
+                if ('MO' == station_info['stations'][i]['station_direct']) and (stop_station == station_info['stations'][i]['station_name']):
                     station_id = station_info['stations'][i]['station_id']
                     break
-            get_bus_detail(begin_station,wait_station,rid,segment_id,station_id)
-        #print(station_info)
-        ''' 
-        print('rid',rid)
-        print('segment_id',segment_id)
-        print('始发站', begin_station)
-        print('等待站', wait_station)
-        print('等待站ID', station_id)
-        print('rid',rid)
-        print('总共',all_station_num,'站')
-        '''       
+            get_bus_detail(departure_station,stop_station,rid,segment_id,station_id)
     else:
-        print(bus_num + '路公交暂无信息')
+        print(rid + '路公交暂无信息')
 
 #获取需要的线路公交到站信息
-def get_bus_detail(begin_station,wait_station,rid,segment_id,station_id):
-    bus_info = ''
+def get_bus_detail(departure_station,stop_station,rid,segment_id,station_id):
+    bus_details = ''
     url = 'http://bus.qingdaonews.com/m/detail_ajax.php?rid=' + rid + '&smid=' + segment_id + '&id=' + station_id + '&from=m'
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 QIHU 360SE'
     }
     response=requests.get(url, headers=headers)
     response.encoding='utf-8'
-    time.sleep(2)
     #Unicode转中文
-    list_right = str(response.text).replace('u\'','\'')
+    list_convert = str(response.text).replace('u\'','\'')
     #转成列表
-    bus_detail = eval(list_right.encode('utf-8').decode("unicode-escape"))
-  
-    print('公交查询', rid + '路', wait_station + '站')
-    print('始发站',begin_station, '\n')
+    bus_detail = eval(list_convert.encode('utf-8').decode("unicode-escape"))
+    print('公交查询', rid + '路', stop_station + '站')
+    print('始发站',departure_station, '\n')
     if 'error' in bus_detail:
         print(bus_detail['error'])
     else:
         for item in bus_detail:
-            for k,v in item.items():
-                if k == 'car_num':
-                    k = '车辆编号'
-                elif k == 'current_station_name':
-                    k = '当前站点'
-                elif k== 'time_to_there':
-                    k = '到站时间'
-                elif k== 'time_to_there2':
-                    k = '到站时间'
-                elif k== 'station_count_remain':
-                    k = '离' + wait_station
-                    v = v + ' 站'
-                elif k== 'last_bus':
-                    k = '末班车'
-                elif k== 'stationseq':
-                    k = '站点顺序'    
-                if (k != '末班车') and (k != '站点顺序'):
+            for key,values in item.items():
+                if key == 'car_num':
+                    k1 = '车辆编号'
+                    v1 = values
+                elif key == 'current_station_name':
+                    k2 = '当前站点'
+                    v2 = values
+                elif key == 'time_to_there':
+                    k3 = '到站时间'
+                    v3 = values
+                elif key == 'time_to_there2':
+                    k4 = '到站时间'
+                    v4 = values
+                elif key == 'station_count_remain':
+                    k5 = '离 ' + stop_station + ' 还有'
+                    v5 = values + ' 站'
+                elif key == 'last_bus':
+                    k6 = '末班车'
+                elif key == 'stationseq':
+                    k7 = '站点顺序'    
+                #if (k != '末班车') and (k != '站点顺序'):
                     #print(k,v,' ',end='')
-                    bus_info += k + ' ' + v + ' '
-            bus_info += '\n'
+                    #bus_info += k + ' ' + v + ' '
+            #bus_info += k1 + ' ' + v1 + ' ' + k2 + ' ' + v2 + ' ' +k3 + ' ' + v3 + ' ' +k4 + ' ' + v4 + ' ' + k5 + ' ' + v5 + ' '
+            #bus_info += '\n'
+            bus_details += rid + '路 ' + v1 + ' ' + v3 + '到达' + ' ' + v2 + ' ' + k5 + ' ' + v5 + ' ' 
+            bus_details += '\n'
             #print()
-    print(bus_info)
+    print(bus_details)
 
     #发送到群晖Chat
-    Synology_Chat(bus_info)
+    #Synology_Chat(bus_details)
 
 #到站信息发送到Synology_Chat,需要在Chat-整合 里传入的Webhook创建一个。
-def Synology_Chat(bus_info):
+def Synology_Chat(bus_details):
     #curl -X POST  --data-urlencode 'payload={"text": "This is a test"}'  "http://192.168.10.254:5000/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22a%22"
     chat_url = "http://192.168.10.254:5000/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22a%22"
-    #payload = 'payload={"text": "This is a test"}'
-    #payload = 'payload={"text": "这是一个测试"}'
     payload = 'payload={"text": "' + bus_info + '"}'
     payload = payload.encode("utf-8").decode("latin1")
     r = requests.post(chat_url, payload).text
 
 if __name__ == '__main__':
-    get_bus_info(bus_num1, begin_station1, wait_station1)
-    #get_bus_info(bus_num2, begin_station2, wait_station2) 
+    get_bus_info(rid, departure_station, stop_station) 
